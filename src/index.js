425c08c795e8515264f5351e34f546eefa99c770
A1ves <ayronwork@gmail.com>
2024-02-07 15:38:39 -0300

    feat: add function to create user

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 const app = express();
 
 app.use(express.json());
 
+const users = []
+
 app.get("/", (request, response) =>{
     return response.status(200).json({"check": "Hellow world !"})
 });
+
+app.post("/account", (request,response) =>{
+    const { userName, password } = request.body
+
+    users.push({
+        userName,
+        password,
+        ID: uuidv4(),
+    });
+
+    return response.status(201).send();
+});
+
 
 app.listen(8000);
