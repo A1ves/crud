7da5a9e58d3df4f2eb56205bdf1a0c94a4705454
A1ves <ayronwork@gmail.com>
2024-02-15 20:49:22 -0300

    fix to vercel file

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
-node: {
-    __dirname: false
-}
-plugins: [
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development', // ou 'production'
+  entry: './src/index.js', // substitua pelo seu ponto de entrada real
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    fallback: {
+      "util": require.resolve("util"),
+      "path": require.resolve("path-browserify"),
+      "url": require.resolve("url"),
+    },
+  },  
+  plugins: [
     new CopyWebpackPlugin({
-        patterns: [
-            './node_modules/swagger-ui-dist/swagger-ui.css',
-            './node_modules/swagger-ui-dist/swagger-ui-bundle.js',
-            './node_modules/swagger-ui-dist/swagger-ui-standalone-preset.js',
-            './node_modules/swagger-ui-dist/favicon-16x16.png',
-            './node_modules/swagger-ui-dist/favicon-32x32.png'
-        ]
-    })
-]
+      patterns: [
+        './node_modules/swagger-ui-dist/swagger-ui.css',
+        './node_modules/swagger-ui-dist/swagger-ui-bundle.js',
+        './node_modules/swagger-ui-dist/swagger-ui-standalone-preset.js',
+        './node_modules/swagger-ui-dist/favicon-16x16.png',
+        './node_modules/swagger-ui-dist/favicon-32x32.png',
+      ],
+    }),
+  ],
+};
